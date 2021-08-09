260f2b89a6db227b87e26b767072f7eed3ed88c8
alexandr.mochalov <alexandr.mochalov@masterofcode.com>
2021-08-09 19:03:27 +0300

    first commit

---
+++ buttons.js
@@ -0,0 +1,26 @@
+function displayTitle(name) {
+  var catTitle = document.createElement('div');
+  catTitle.classList.add('top-title');
+  catTitle.innerText = name;
+  document.getElementById('cat-title').appendChild(catTitle);
+}
+
+function createMenu() {
+  [
+    { name: 'DEV Home Page', section: 'site-gs-dev-homepage' },
+    { name: 'DEV Kitchen', section: 'site-gs-dev-kitchen' },
+    { name: 'STG Home Page', section: 'site-gs-stg-homepage' },
+    { name: 'STG Kitchen', section: 'site-gs-stg-kitchen' },
+  ].forEach(function(cat) {
+    var aTag = document.createElement('a');
+    aTag.setAttribute('href', '?section=' + cat.section);
+    aTag.setAttribute('id', 'category-btn-' + cat.section);
+    if (section === cat.section) {
+      aTag.classList.add('active');
+      displayTitle(cat.name);
+    }
+    aTag.innerText = cat.name;
+    document.getElementById('top-menu').appendChild(aTag)
+  });
+};
+
---
+++ sections.js
@@ -0,0 +1,2 @@
+var defaultSection = "site-gs-stg-homepage";
+var section = new URL(window.location).searchParams.get('section') || defaultSection;
