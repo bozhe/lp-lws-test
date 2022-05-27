eb6b2644eac1f757361a9c88a3998a8011db3d6b
oleksandr.mochalov <oleksandr.mochalov@gmail.com>
2022-05-27 16:38:48 +0300

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,21 @@
+const categories = [
+  { name: 'Dev', items: [
+    { name: 'DEV Home Page', section: 'site-gs-dev-homepage' },
+    { name: 'DEV Kitchen', section: 'site-gs-dev-kitchen' },
+    { name: 'DEV P.Match', section: 'site-gs-dev-price-promise' },
+    { name: 'DEV SMS Home', section: 'site-gs-dev-sms-homepage' },
+  ]},
+  { name: 'Stage', items: [
+    { name: 'STG Home Page', section: 'site-gs-stg-homepage' },
+    { name: 'STG Kitchen', section: 'site-gs-stg-kitchen' },
+    { name: 'STG P.Match', section: 'site-gs-stg-price-promise' },
+    { name: 'STG SMS Home', section: 'site-gs-stg-sms-homepage' },
+  ]},
+  { name: 'Pro Loyalty', items: [
+    { name: 'STG PRO 1', section: 'site-gs-stg-pro-loyalty-1' },
+    { name: 'STG PRO 2', section: 'site-gs-stg-pro-loyalty-2' },  
+    { name: 'STG ABC PRO', section: 'abc-gs-stg-pro-loyalty' },
+    { name: 'STG SMS PRO', section: 'sms-gs-stg-pro-loyalty' },
+  ]},
+]
+
