dfb0275a45aa20809cad3b53931f7a91f9e37683
oleksandr.mochalov <bozhe@ukr.net>
2022-07-04 10:48:26 +0300

    Updare regex generator

--- regex-gen.js
+++ regex-gen.js
@@ -1,11 +1,42 @@
 function onGenerateClick() {
+  const objRegex = /\{.*title:\s*['"`](.*)['"`]\s*(,\s.*|\s*)\},?/;
+  const strRegex = /^['"`](.*)['"`],?$/;
+
+  const yesPattern = '(yes|right|of course|by all means|sure|certainly|absolutely|indeed|agreed|aye|yeah|yah|yep|yup|okay|ok|okey-dokey|okey-doke|surely)(([\\s.,-]*)?th.*nk.*)?';
+  const noPattern = '(absolut.*|total.*)?((no(t)?.*(way|right|really|entirely|exactly|thank.*)?|nah|nope|wrong|disagree|doubt.*))';
+  const allSetPattern = '(i(\\s+am|.{0,1}m)\\s+)?(all\\s+)?(done|good|ok|set)';
+  const chatPattern = '(chat|talk|speak|connect)\\s(with|to).*(associate|human|agent)';
+  const menuPattern = '(main\\s+?)?menu';
+
+  const yesRegex = /(yes|right|of course|by all means|sure|certainly|absolutely|indeed|agreed|aye|yeah|yah|yep|yup|okay|ok|okey-dokey|okey-doke|surely)(([\s.,-]*)?th.*nk.*)?/i;
+  const noRegex = /(absolut.*|total.*)?((no(t)?.*(way|right|really|entirely|exactly|thank.*)?|nah|nope|wrong|disagree|doubt.*))/i;
+  const allSetRegex = /(i(\s+am|.{0,1}m)\s+)?(all\s+)?(done|good|ok|set)/i;
+  const chatRegex = /(chat|talk|speak|connect)\s(with|to).*(associate|human|agent)/i;
+  const menuRegex = /(main\s+?)?menu/i;
+
   const textArea = window.document.getElementById('regex-options-text');
   const regexes = (textArea.value || '').split('\n')
-    .map(v => v.replace(/^([a-z1-9]\W{1,3})?/i, '').replace(/[^a-z0-9 ]/ig, '.').trim())
+    .map(l => {
+      let line = l.trim().replace(/^.*\[/, '').replace(/\].*$/, '');
+      if (objRegex.test(line)) line = line.match(objRegex)[1];
+      if (strRegex.test(line)) line = line.match(strRegex)[1];
+      return line.replace(/^([a-z1-9]\W{1,3})?/i, '')
+        .replace(/\\['"`]/g, '.')
+        .replace(/[^a-z0-9 ]/ig, '.')
+        .replace(/\.{2,}/g, '.+')
+        .toLowerCase()
+        .trim();
+    })
     .filter(v => !!v)
     .map((opt, index) => {
       const char = 'abcdefghij'[index];
-      return `(?i)^([${char}${index+1}]\\W{0,3})?(${opt})?$`
+      const optPattern = yesRegex.test(opt) ? yesPattern :
+        (noRegex.test(opt) ? noPattern : 
+        (allSetRegex.test(opt) ? allSetPattern :
+        (chatRegex.test(opt) ? chatPattern :
+        (menuRegex.test(opt) ? menuPattern :
+        opt))));
+      return `(?i)^([${char}${index+1}]\\W{0,3})?(${optPattern})?$`
     });
   createRegexLines(regexes);
 }
@@ -21,23 +52,24 @@ function createRegexLines(regexes) {
   parent.innerHTML = '';
   regexes.forEach(r => {
     const row = document.createElement('div');
-    const valueCell = document.createElement('div');
-    const buttonCell = document.createElement('div');
-    const button = document.createElement('img');
-
     row.classList.add('row');
-    valueCell.classList.add('cell', 'code');
-    buttonCell.classList.add('cell');
+    parent.appendChild(row);
 
-    valueCell.innerText = r;
+    const buttonCell = document.createElement('div');
+    buttonCell.classList.add('cell');
+    row.appendChild(buttonCell);
+
+    const button = document.createElement('img');
     button.classList.add('copy-icon');
     button.setAttribute('src', 'copy.svg');
     button.onclick = () => { copyRegex(r, textarea); };
-
-    parent.appendChild(row);
-    row.appendChild(valueCell);
-    row.appendChild(buttonCell);
     buttonCell.appendChild(button);
+    
+    const valueCell = document.createElement('div');
+    valueCell.classList.add('cell', 'code');
+    valueCell.innerText = r;
+    valueCell.ondblclick = () => { copyRegex(r, textarea); };
+    row.appendChild(valueCell);
   });
 }
 
